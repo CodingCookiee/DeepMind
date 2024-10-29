54115206de976f525fc6f52828a0dc82e703198d
CodingCookiee <rzqutubawan@gmail.com>
2024-10-29 20:59:28 +0500

    Google Gemini Integration

--- client/src/Components/ChatList.jsx
+++ client/src/Components/ChatList.jsx
@@ -1,41 +1,52 @@
-import './chatList.css';
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import ThemeContext from '../ThemeContext';
+import "./chatList.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import ThemeContext from "../ThemeContext";
 
 const ChatList = () => {
-    const { theme } = useContext(ThemeContext);
-    const hoverBg = theme === 'light' ? 'hover:bg-slate-200' : 'hover:bg-neutral-800';
-    const scrollbarClass = theme === 'dark' ? 'scrollbar-dark' : 'scrollbar-light';
+  const { theme } = useContext(ThemeContext);
+  const hoverBg =
+    theme === "light" ? "hover:bg-slate-200" : "hover:bg-neutral-800";
+  const scrollbarClass =
+    theme === "dark" ? "scrollbar-dark" : "scrollbar-light";
 
-    return (
-        <div className="chatList flex flex-col p-5 h-5/6">
-            <span className="title font-semibold text-xs mb-2.5">DASHBOARD</span>
-            <hr className="border-none h-0.5 bg-slate-50 opacity-10 rounded-sm mb-5" />
+  return (
+    <div className="chatList flex flex-col p-5 h-5/6">
+      <span className="title font-semibold text-xs mb-2.5">DASHBOARD</span>
+      <Link to="/dashboard" className="font-normal text-sm mb-2.5">
+        Create a new Chat
+      </Link>
+      <Link to="/" className="font-normal text-sm mb-2.5">
+        Explore Lama AI
+      </Link>
+      <Link to="/" className="font-normal text-sm mb-2.5">
+        Contact
+      </Link>
+      <hr className="border-none h-0.5 bg-slate-50 opacity-10 rounded-sm mb-5" />
 
-            <span className="title font-semibold text-xs mb-2.5">RECENT CHATS</span>
-            <div className={`list flex flex-col overflow-y-auto flex-grow h-full ${scrollbarClass}`}>
-                {Array.from({ length: 20 }).map((_, idx) => (
-                    <Link key={idx} to="/" className={`p-2.5 rounded-xl ${hoverBg}`}>
-                        Chat Title {idx + 1}
-                    </Link>
-                ))}
-            </div>
-            <hr className="border-none h-0.5 bg-slate-50 opacity-10 rounded-sm mt-5 mb-5" />
-          
-            <div className="upgrade -mb-16 mt-auto flex items-center gap-2 text-xs ">
-                <img
-                    src='/panda.svg'
-                    alt=""
-                    className="w-6 h-6 mb-8"
-                />
-                <div className="texts flex flex-col gap-1">
-                    <span className="font-semibold">Upgrade to PANDA AI Pro</span>
-                    <span className="text-slate-500">Get unlimited access to all features</span>
-                </div>
-            </div>
+      <span className="title font-semibold text-xs mb-2.5">RECENT CHATS</span>
+      <div
+        className={`list flex flex-col overflow-y-auto flex-grow h-full ${scrollbarClass}`}
+      >
+        {Array.from({ length: 20 }).map((_, idx) => (
+          <Link key={idx} to="/" className={`p-2.5 rounded-xl ${hoverBg}`}>
+            Chat Title {idx + 1}
+          </Link>
+        ))}
+      </div>
+      <hr className="border-none h-0.5 bg-slate-50 opacity-10 rounded-sm mt-5 mb-5" />
+
+      <div className="upgrade -mb-16 mt-auto flex items-center gap-2 text-xs ">
+        <img src="/panda.svg" alt="" className="w-6 h-6 mb-8" />
+        <div className="texts flex flex-col gap-1">
+          <span className="font-semibold">Upgrade to PANDA AI Pro</span>
+          <span className="text-slate-500">
+            Get unlimited access to all features
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ChatList;
--- client/src/Components/newPrompt/newPrompt.jsx
+++ client/src/Components/newPrompt/newPrompt.jsx
@@ -4,14 +4,10 @@ import React, { useContext, useRef, useEffect, useState } from "react";
 import ThemeContext from "../../ThemeContext";
 import { IKContext, IKImage, IKUpload } from "imagekitio-react";
 import Upload from "../upload/uploadData";
+import model from "../../lib/gemini.js";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
-  const [img, setImg] = useState({
-    isLoading: false,
-    error: false,
-    dbData: {},
-  });
-
   const { theme } = useContext(ThemeContext);
   const placeholderClasses =
     theme === "dark"
@@ -22,16 +18,68 @@ const NewPrompt = () => {
       ? "bg-neutral-800 text-white hover:bg-neutral-600"
       : "bg-slate-200 text-black hover:bg-slate-300";
   const newFormClasses = theme === "dark" ? "bg-neutral-700" : "bg-white";
+  const userBg = theme === "light" ? "bg-slate-300" : "bg-neutral-700";
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
+  });
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      // maxOutputTokens: 100,
+    },
+  });
+
   const endRef = useRef(null);
 
-  const [imageLoaded, setImageLoaded] = useState(false);
-
   useEffect(() => {
-    if (endRef.current) {
-      endRef.current.scrollIntoView({ behavior: "smooth" });
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [img.dbData, img.isLoading, question, answer]);
+
+  const add = async (text) => {
+    setQuestion(text);
+
+    const result = await chat.sendMessageStream(
+      Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+    );
+    let accumulatedText = '';
+    for await (const chunk of result.stream){
+      const chunkText = chunk.text();
+      accumulatedText += chunkText
+      setAnswer(accumulatedText);
     }
-  }, [img.isLoading, imageLoaded]);
-  
+    
+    setImg({
+      isLoading: false,
+      error: "",
+      dbData: {},
+      aiData: {},
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+  };
+
   return (
     <>
       {img.isLoading && (
@@ -55,17 +103,30 @@ const NewPrompt = () => {
             progressive: true,
           },
         ]}
-        className="mt-4"
-        onLoad={() => setImageLoaded(prev => !prev)}
+        className="mt-4 mb-4"
       />
+      {question && (
+        <div
+          className={`message user p-5 rounded-3xl max-w-[90%] self-end ml-auto ${userBg}`}
+        >
+          {question}
+        </div>
+      )}
+      {answer && (
+        <div className="message p-5">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="endChat p-10" ref={endRef}></div>
       <form
         className={`newForm flex items-center w-6/12 gap-3 max-w-2xl 
-                px-4 py-2 rounded-full absolute -bottom-2 ${newFormClasses}`}
+        px-4 py-2 rounded-full absolute -bottom-2 ${newFormClasses}`}
+        onSubmit={handleSubmit}
       >
         <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
         <input
+          name="text"
           type="text"
           placeholder="Ask me Anything ..."
           className={`flex-grow p-3 rounded-full focus:outline-none ${placeholderClasses}`}
@@ -80,4 +141,5 @@ const NewPrompt = () => {
     </>
   );
 };
+
 export default NewPrompt;
