9314d9fc4362cf3f9ac56fde84cbfd77a97d4544
CodingCookiee <rzqutubawan@gmail.com>
2024-11-04 21:36:49 +0500

    Commit Changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import { imagekitInstance } from "./config/imagekit.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
