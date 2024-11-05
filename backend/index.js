8004e1c5906497c5b68e2398bc88cdd7d6c37f37
CodingCookiee <rzqutubawan@gmail.com>
2024-11-05 10:33:03 +0500

    Commit Changes

--- backend/index.js
+++ backend/index.js
@@ -33,11 +33,10 @@ app.get("/api/upload", (req, res) => {
 
 app.use("/api", chatRoutes);
 
-app.use(express.static(path.join(__dirname, '../client')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../client')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client', 'index.html'));
+// });
 
 
 app
