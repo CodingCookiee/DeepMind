1d7c460027b860e3849f5813a9335a87188ea289
CodingCookiee <rzqutubawan@gmail.com>
2024-11-05 13:25:46 +0500

    Commit Changes

--- backend/index.js
+++ backend/index.js
@@ -43,10 +43,11 @@ app.get("/api/upload", (req, res) => {
 app.use("/api", chatRoutes);
 
 // Serve Static Files for Frontend
-// app.use(express.static(path.join(__dirname, "../client")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+
 
 // Global Error Handler
 app.use((err, req, res, next) => {
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { requireAuth } from "../middleware/authMiddleware.js";
+// import  from "../middleware/authMiddleware.js";
 import {
   createChat,
   fetchChatTitle,
@@ -12,12 +12,12 @@ import {
 
 const router = express.Router();
 
-router.post("/chats", requireAuth(), createChat);
-router.get("/chats/:id/title", requireAuth(), fetchChatTitle);
-router.get("/userChats", requireAuth(), fetchUserChats);
-router.get("/chats/:id", requireAuth(), fetchSpecificChatHistory);
-router.put("/chats/:id", requireAuth(), updateChat);
-router.put("/chats/:id/title", requireAuth(), editChatTitle);
-router.delete("/chats/:id", requireAuth(), deleteChat);
+router.post("/chats", createChat);
+router.get("/chats/:id/title", fetchChatTitle);
+router.get("/userChats", fetchUserChats);
+router.get("/chats/:id", fetchSpecificChatHistory);
+router.put("/chats/:id", updateChat);
+router.put("/chats/:id/title", editChatTitle);
+router.delete("/chats/:id", deleteChat);
 
 export default router;
